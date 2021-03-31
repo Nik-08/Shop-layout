f4a05ff514eaf74382d963b60fd3fd24423c84cc
Nik-08 <69086824+Nik-08@users.noreply.github.com>
2021-03-31 17:33:29 +0700

    desktop version

---
+++ script/script.js
@@ -0,0 +1,28 @@
+const product = document.querySelector('.product'),
+productItem = document.querySelectorAll('.product_item');
+
+
+
+const changeImg = () => {
+  productItem.forEach((i) => {
+    let img = i.querySelector('.img');
+    product.addEventListener('mouseover', (event) => {
+      let target = event.target;
+      if(target.closest('.product_item') === i){
+        img.style.opacity = 1;
+      }else {
+        img.style.opacity = 0;
+      }
+    });
+    product.addEventListener('mouseover', (event) => {
+      let target = event.target;   
+      if(target === product) {
+        img.style.opacity = 0;
+      }   
+    });
+  });
+};
+changeImg();
+
+
+
